2dad4dd06d2710c32c742c286714757f82d12e62
corbat <corbat@verticalfluent.com>
2014-04-11 11:43:13 -0500

    Updating name references of "rubber" to "deploy"

--- rails/init.rb
+++ rails/init.rb
@@ -1,6 +1,6 @@
-# this file just makes it easy to refer to rubber config
+# this file just makes it easy to refer to deploy config
 # variables in your apps's config'
 #
 
-require "rubber"
+require "deploy"
 Rubber::initialize(Rails.root, Rails.env)
